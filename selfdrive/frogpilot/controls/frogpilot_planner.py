bfbbc3bb70a4278fe506543a05e55e00da230fd5
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-07 07:35:36 -0700

    Controls - Conditional Experimental Mode - Slower Lead Detected Ahead
    Switch to 'Experimental Mode' when a slower lead vehicle is detected ahead.

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -27,6 +27,8 @@ class FrogPilotPlanner:
 
     self.cem = ConditionalExperimentalMode()
 
+    self.slower_lead = False
+
     self.acceleration_jerk = 0
     self.frame = 0
     self.road_curvature = 0
@@ -79,6 +81,11 @@ class FrogPilotPlanner:
     self.frame += DT_MDL
 
   def update_follow_values(self, lead_distance, stopping_distance, v_ego, v_lead, frogpilot_toggles):
+    # Offset by FrogAi for FrogPilot for a more natural approach to a slower lead
+    if frogpilot_toggles.conditional_experimental_mode and v_lead < v_ego:
+      distance_factor = max(lead_distance - (v_lead * self.t_follow), 1)
+      braking_offset = np.clip((v_ego - v_lead) - COMFORT_BRAKE, 1, distance_factor)
+      self.slower_lead = max(braking_offset, 1) > 1
 
   def update_v_cruise(self, carState, controlsState, frogpilotCarState, frogpilotNavigation, modelData, v_cruise, v_ego, frogpilot_toggles):
     v_cruise_cluster = max(controlsState.vCruiseCluster, v_cruise) * CV.KPH_TO_MS
