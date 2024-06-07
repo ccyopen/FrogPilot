b22074b3635cd12db871f0d7cf6658dc6b290043
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-07 02:58:29 -0700

    FrogPilot setup - Configure cereal

--- selfdrive/ui/qt/onroad/onroad_home.cc
+++ selfdrive/ui/qt/onroad/onroad_home.cc
@@ -124,6 +124,9 @@ void OnroadWindow::primeChanged(bool prime) {
 }
 
 void OnroadWindow::paintEvent(QPaintEvent *event) {
+  UIState *s = uiState();
+  SubMaster &sm = *(s->sm);
+
   QPainter p(this);
   p.fillRect(rect(), QColor(bg.red(), bg.green(), bg.blue(), 255));
 }
