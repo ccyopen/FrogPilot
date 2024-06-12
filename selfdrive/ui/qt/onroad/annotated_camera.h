75bb32f304b63edff61814ddc7b22029dbe2cc65
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-12 16:38:21 -0700

    Visuals - Custom Onroad UI - Paths - Adjacent
    Show the detected adjacent lanes.

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -81,6 +81,8 @@ private:
   QHBoxLayout *bottom_layout;
 
   bool alwaysOnLateralActive;
+  bool blindSpotLeft;
+  bool blindSpotRight;
   bool compass;
   bool experimentalMode;
   bool mapOpen;
@@ -99,6 +101,9 @@ private:
   float accelerationConversion;
   float cruiseAdjustment;
   float distanceConversion;
+  float laneDetectionWidth;
+  float laneWidthLeft;
+  float laneWidthRight;
   float slcSpeedLimitOffset;
   float speedConversion;
   float unconfirmedSpeedLimit;
